fc3bb530024b657db33660e8eab65a03786dd71e
Taylor Rodgers <taylor@taylorrodgers.com>
2024-10-24 07:54:11 -0500

    #4: word doc refinement. Not yet complete. Shifting to posit.cloud for easier system dependency management.

--- R/plot_area_q.R
+++ R/plot_area_q.R
@@ -10,12 +10,12 @@
 #' @export
 #' @examples
 #' data("df_summarized_response_example")
-#' plot_area_q(df_summarized_response = df_summarized_response_example)
+#' plot_area_q(df_summarized_response = df_summarized_response_example[1:2,], v_primary_date = "2024-08-23")
 #'
 
 
 
-plot_area_q <- function(df_summarized_response, v_primary_date) {
+plot_area_q <- function(df_summarized_response, v_primary_date, doc_version_ind = FALSE) {
 
   if (nrow(df_summarized_response) > 1 && is.null(v_primary_date)) {
     stop("v_primary_date is required when df_summarized_response has more than one row.")
@@ -44,16 +44,37 @@ plot_area_q <- function(df_summarized_response, v_primary_date) {
     colors <- "#338080E6"
   }
 
-  ggradar(
-    dat_for_plot,
-    values.radar = c(0, grid_size / 2, grid_size),
-    grid.min = 0,
-    grid.mid = grid_size / 2,
-    grid.max = grid_size,
-    axis.label.size = 3.5,
-    group.colours = colors,
-    legend.position = "bottom"
-  )
+  if (doc_version_ind) {
+        p <- ggradar(
+      dat_for_plot,
+      values.radar = c(0, grid_size / 2, grid_size),
+      grid.min = 0,
+      grid.mid = grid_size / 2,
+      grid.max = grid_size,
+      axis.label.size = 3.5,
+      group.colours = colors,
+      legend.position = "bottom"
+    )
+    p <- ggradar(
+      dat_for_plot,
+      base.size = 9,
+      values.radar = c(0, grid_size / 2, grid_size),
+      grid.min = 0,
+      grid.mid = grid_size / 2,
+      grid.max = grid_size,
+      grid.line.width = .25,
+      group.line.width = .75,
+      group.point.size = 3,
+      axis.label.size = 2,
+      legend.text.size = 9,
+      grid.label.size = 2,
+      group.colours = colors,
+      legend.position = "bottom"
+    )
+  } else {
 
+  }
+
+  return(p)
 
 }
